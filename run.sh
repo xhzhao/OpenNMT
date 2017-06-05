b78f33bd5ce4fd5ab01d8b25937a938a78877731
Xiaohui Zhao <xiaohui.zhao@intel.com>
2017-06-05 16:51:26 +0800

    add vtune

--- run.sh
+++ run.sh
@@ -2,3 +2,5 @@
 source ~/.bashrc
 th train.lua -data data/demo-train.t7 -save_model model -profiler true
 
+#~/intel/vtune_amplifier_xe/bin64/amplxe-cl -collect hotspots -knob analyze-openmp=true -knob sampling-interval=30 --resume-after 8 -d 50  th train.lua -data data/demo-train.t7 -save_model model -profiler true
+
