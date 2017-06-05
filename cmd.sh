d6c896676318682d3ed2e4ab25a9fad2aed70b88
Xiaohui Zhao <xiaohui.zhao@intel.com>
2017-06-05 17:02:06 +0800

    add cmd info

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,8 @@
 grep "iteration" slurm-26191.out
 
+grep iteration slurm-6553.out | wc -l
+grep iteration slurm-6553.out | awk '{sum+=$4} END {print sum}'
+
 ./profile.sh slurm-26191.out | awk '{print $3}'
 
 ./profile.sh slurm-26191.out &> tmp && cat tmp | awk '{sum+=$3} { print sum}'
